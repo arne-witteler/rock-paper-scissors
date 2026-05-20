25e83c9fb0bb39fb58cd44cfacc57cd254e45e7a
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-05-20 17:05:55 +0200

    add computer choice

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+  const random = Math.floor(Math.random() * 3);
+
+  if (random === 0) {
+    return "rock";
+  } else if (random === 1) {
+    return "paper";
+  } else
+    return "scissor";
+}
+
