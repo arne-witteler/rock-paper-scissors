a7f75e541da3a6d937efd6bb9af39c988633974f
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-05-21 09:45:30 +0200

    implement game logic and input validation for rock-paper-scissors

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
   const random = Math.floor(Math.random() * 3);
 
@@ -12,10 +15,33 @@ function getComputerChoice() {
 function getHumanChoice() {
   const validInput = ["rock", "paper", "scissors"];
 
-  let input = prompt("What do you choose?");
+  let input = prompt("What do you choose?").toLowerCase();
 
   if (validInput.includes(input)) {
     return input;
   } else
     return "Invalid Input"
 }
+
+function playRound(humanChoice, computerChoice) {
+
+  if (humanChoice === "Invalid Input") {
+    console.log("Invalid Input")
+    return;
+  }
+
+  if (humanChoice === computerChoice) {
+    return "Draw";
+  } else if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore++
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+  } else {
+    humanScore++
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
