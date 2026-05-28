604ffdee8f1368ad289c6c8caf14d5d289eaf6c8
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-05-28 14:30:20 +0200

    implement game logic and update UI for player choices and scoring

--- script.js
+++ script.js
@@ -1,3 +1,90 @@
+const startScreen = document.querySelector(".screen--start");
+const gameScreen = document.querySelector(".screen--game");
+const endScreen = document.querySelector(".screen--end");
+
+const playButton = document.querySelector("#start-game-button");
+const playAgainButton = document.querySelector("#play-again-button");
+const mainMenuButton = document.querySelector("#main-menu-button")
+
+const rockButton = document.querySelector("#rock-button");
+const paperButton = document.querySelector("#paper-button");
+const scissorsButton = document.querySelector("#scissors-button");
+
+const choiceButtons = document.querySelectorAll(".screen__buttons");
+const currentRound = document.querySelector("#current-round");
+
+const playerPoints = document.querySelector("#player-score");
+const computerPoints = document.querySelector("#computer-score");
+
+const finalPlayerPoints = document.querySelector("#final-player-score");
+const finalComputerPoints = document.querySelector("#final-computer-score");
+
+const gameMessage = document.querySelector(".screen__message");
+const gameOverMessage = document.querySelector("#game-over-text");
+
+const playerChoiceImg = document.querySelector("#player-choice-img");
+const computerChoiceImg = document.querySelector("#computer-choice-img");
+
+let roundCounter = 1;
+const maxRounds = 5;
+let humanScore = 0;
+let computerScore = 0;
+
+playButton.addEventListener("click", () => {
+  startScreen.classList.add("hidden");
+  gameScreen.classList.remove("hidden");
+})
+
+playAgainButton.addEventListener("click", () => {
+  roundCounter = 1;
+  humanScore = 0;
+  computerScore = 0;
+
+  currentRound.textContent = roundCounter;
+  playerPoints.textContent = humanScore;
+  computerPoints.textContent = computerScore;
+
+  endScreen.classList.add("hidden");
+  gameScreen.classList.remove("hidden");
+})
+
+mainMenuButton.addEventListener("click", () => {
+  roundCounter = 1;
+  humanScore = 0;
+  computerScore = 0;
+
+  currentRound.textContent = roundCounter;
+  playerPoints.textContent = humanScore;
+  computerPoints.textContent = computerScore;
+
+  endScreen.classList.add("hidden");
+  startScreen.classList.remove("hidden");
+})
+
+rockButton.addEventListener("click", () => {
+  let input = "rock";
+  let computerChoice = getComputerChoice();
+  roundCounter++;
+  currentRound.textContent = roundCounter;
+  playRound(input, computerChoice);
+})
+
+paperButton.addEventListener("click", () => {
+  let input = "paper";
+  let computerChoice = getComputerChoice();
+  roundCounter++;
+  currentRound.textContent = roundCounter;
+  playRound(input, computerChoice);
+})
+
+scissorsButton.addEventListener("click", () => {
+  let input = "scissors";
+  let computerChoice = getComputerChoice();
+  roundCounter++;
+  currentRound.textContent = roundCounter;
+  playRound(input, computerChoice);
+})
+
 function getComputerChoice() {
   const random = Math.floor(Math.random() * 3);
 
@@ -9,44 +96,48 @@ function getComputerChoice() {
     return "scissors";
 }
 
-function getHumanChoice() {
-  const validInput = ["rock", "paper", "scissors"];
+function playRound(humanChoice, computerChoice) {
 
-  let input = prompt("What do you choose?").toLowerCase();
+  if (roundCounter < maxRounds) {
 
-  if (validInput.includes(input)) {
-    return input;
-  } else
-    return "Invalid Input"
-}
+    playerChoiceImg.src = `assets/hand-${humanChoice}.svg`;
+    computerChoiceImg.src = `assets/hand-${computerChoice}.svg`;
 
-function playGame() {
-
-  const rounds = 5;
-
-  let computerScore = 0;
-  let humanScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "Invalid Input") {
-      console.log("Invalid Input");
-      return;
-    }
-    
-    if (humanChoice === computerChoice) {
-      return "Draw";
-    } else if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock") {
-      computerScore++;
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      if (humanChoice === "Invalid Input") {
+        console.log("Invalid Input");
+        return;
+      }
+      
+      if (humanChoice === computerChoice) {
+        gameMessage.classList.remove("hidden");
+        gameMessage.textContent = "It's a draw!".toUpperCase();
+      } else if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        currentRound.textContent = roundCounter;
+        computerPoints.textContent = computerScore;
+        gameMessage.classList.remove("hidden");
+        gameMessage.textContent = `You lose! ${computerChoice} beats ${humanChoice}!`.toUpperCase();
+        
+      } else {
+        humanScore++;
+        currentRound.textContent = roundCounter;
+        playerPoints.textContent = humanScore;
+        gameMessage.classList.remove("hidden");
+        gameMessage.textContent = `You win! ${humanChoice} beats ${computerChoice}!`.toUpperCase();
+        
+      }
     } else {
-      humanScore++;
-      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      endScreen.classList.remove("hidden");
+      gameScreen.classList.add("hidden");
+      finalPlayerPoints.textContent = humanScore;
+      finalComputerPoints.textContent = computerScore;
+
+      if (humanScore > computerScore) {
+        gameOverMessage.textContent = "Congratulations! You won the match!";
+      } else if (computerScore > humanScore) {
+        gameOverMessage.textContent = "Too bad! The CPU won the match!";
+      } else {
+        gameOverMessage.textContent = "It's a tie!";
+      }
     }
   }
-  
-  for (let i = 0; i < rounds; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
-}
